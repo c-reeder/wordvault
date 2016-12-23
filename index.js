7ce341beb6172d6b8939a676ae42a20469d0331b
Connor Reeder <connor.reeder@ymail.com>
2016-12-23 07:28:04 -0500

    Added longer example word list

--- index.js
+++ index.js
@@ -6,7 +6,28 @@ app.get('/', function (req, res) {
 })
 
 app.get('/passwords', function (req, res) {
-	var wordArray = ["Scrumptious", "Wonderful", "Delicious", "Handsome"];
+	var wordArray = [
+		"cat", "sun", "cup",
+		"ghost", "flower", "pie",
+		"cow", "banana", "snowflake",
+		"bug", "book", "jar",
+		"snake", "light", "tree",
+		"lips", "apple", "slide",
+		"socks", "smile", "swing",
+		"coat", "shoe", "water",
+		"heart", "hat", "ocean",
+		"kite", "dog", "mouth",
+		"milk", "duck", "eyes",
+		"skateboard", "bird", "boy",
+		"apple", "person", "girl",
+		"mouse", "ball", "house",
+		"star", "nose", "bed",
+		"whale", "jacket", "shirt",
+		"hippo", "beach", "egg",
+		"face", "cookie", "cheese",
+		"ice", "cream", "cone drum circle",
+		"spoon", "worm", "spider web"
+	];
 	res.send(JSON.stringify(wordArray));
 })
 
