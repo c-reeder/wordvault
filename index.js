db2061ab1ddfd187a8de9eb6bf549372c7e035ab
Connor Reeder <connor.reeder@ymail.com>
2017-01-15 18:16:34 -0500

    Added support for all difficulties

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ app.set('view engine', 'html');
 
 var connString = process.env.DATABASE_URL || "postgres://localhost/mylocaldb";
 
-app.get('/passwords', function (request, response) {
+app.get('/passwords/:difficulty', function (request, response) {
+	var diff = request.params.difficulty;
+	if (diff == "easy" || diff == "medium" || diff == "hard")
   pg.connect(connString, function(err, client, done) {
-    client.query('SELECT * FROM words WHERE difficulty = \'Easy\' ORDER BY random() LIMIT 20;'
+    client.query('SELECT * FROM words WHERE difficulty = \'' + request.params.difficulty + '\' ORDER BY random() LIMIT 20;'
 	    , function(err, result) {
       done();
       if (err)
@@ -21,10 +23,12 @@ app.get('/passwords', function (request, response) {
 	       for (var i = 0; i < result.rows.length; i++) {
 			retrievedWords.push(result.rows[i]["word"]);
 	       }
-	       response.send(JSON.stringify(retrievedWords));
+	       response.send(JSON.stringify(retrievedWords)); 
        }
     });
   });
+	else
+		response.send("Invalid Difficulty!");
 });
 
 app.get('/', function (req, res) {
@@ -37,5 +41,5 @@ var server = app.listen(process.env.PORT, function () {
    var host = server.address().address
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("wordvault app listening at http://%s:%s", host, port)
 })
