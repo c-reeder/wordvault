54906f687a04af7f2a2f5b41b8e47aca42536736
Connor Reeder <connor.reeder@ymail.com>
2017-01-15 16:09:08 -0500

    reverted

--- index.js
+++ index.js
@@ -7,34 +7,9 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 
-app.get('/passwords/easy', function (request, response) {
-	var words = getPasswords("Easy");
-	response.send(JSON.stringify(words));
-
-});
-
-app.get('/passwords/medium', function (request, response) {
-	var words = getPasswords("Medium");
-	response.send(JSON.stringify(words));
-
-});
-
-app.get('/passwords/hard', function (request, response) {
-	var words = getPasswords("Hard");
-	response.send(JSON.stringify(words));
-
-});
-
-app.get('/', function (req, res) {
-	res.send("Default Stuff");
-	
-})
-
-function getPasswords(diff) {
+app.get('/passwords', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM words WHERE difficulty = \''
-	    + diff
-	    + '\' ORDER BY random() LIMIT 20;'
+    client.query('SELECT * FROM words WHERE difficulty = \'Easy\' ORDER BY random() LIMIT 20;'
 	    , function(err, result) {
       done();
       if (err)
@@ -45,11 +20,16 @@ function getPasswords(diff) {
 	       for (var i = 0; i < result.rows.length; i++) {
 			retrievedWords.push(result.rows[i]["word"]);
 	       }
-	       return retrievedWords;
+	       response.send(JSON.stringify(retrievedWords));
        }
     });
   });
-}
+});
+
+app.get('/', function (req, res) {
+	res.send("Default Stuff");
+	
+})
 
 
 var server = app.listen(process.env.PORT, function () {
