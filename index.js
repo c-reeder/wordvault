03e9bcd6c6f28e7368cfa5dfc4616f4e3b890d4d
Connor Reeder <connor.reeder@ymail.com>
2017-01-15 15:50:18 -0500

    Added Difficulty paths

--- index.js
+++ index.js
@@ -7,9 +7,34 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 
-app.get('/passwords', function (request, response) {
+app.get('/passwords/easy', function (request, response) {
+	var words = getPasswords("Easy");
+	response.send(JSON.stringify(words));
+
+});
+
+app.get('/passwords/medium', function (request, response) {
+	var words = getPasswords("Medium");
+	response.send(JSON.stringify(words));
+
+});
+
+app.get('/passwords/hard', function (request, response) {
+	var words = getPasswords("Hard");
+	response.send(JSON.stringify(words));
+
+});
+
+app.get('/', function (req, res) {
+	res.send("Default Stuff");
+	
+})
+
+function getPasswords(diff) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM words WHERE difficulty = \'Easy\' ORDER BY random() LIMIT 20;'
+    client.query('SELECT * FROM words WHERE difficulty = \''
+	    + diff
+	    + '\' ORDER BY random() LIMIT 20;'
 	    , function(err, result) {
       done();
       if (err)
@@ -20,16 +45,11 @@ app.get('/passwords', function (request, response) {
 	       for (var i = 0; i < result.rows.length; i++) {
 			retrievedWords.push(result.rows[i]["word"]);
 	       }
-	       response.send(JSON.stringify(retrievedWords));
+	       return retrievedWords;
        }
     });
   });
-});
-
-app.get('/', function (req, res) {
-	res.send("Default Stuff");
-	
-})
+}
 
 
 var server = app.listen(process.env.PORT, function () {
