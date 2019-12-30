d8c840a8d004f68f66cc4c871edf853e2ad1d75e
Connor Reeder <connor.reeder@ymail.com>
2019-12-30 11:25:39 -0300

    Fixed new posgres api

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var app = express();
 
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
 var connString = process.env.DATABASE_URL || "postgres://localhost/wordvault";
 
-const client = new Client({
+const pool = new Pool({
 	connectionString: connString,
 	ssl: true,
 });
-client.connect();
+
+pool.on('error', (err, client) => {
+	console.error('Unexpected error on idle client', err)
+	process.exit(-1)
+})
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -23,18 +27,21 @@ app.get('/words/:language/:difficulty', function (request, response) {
 		response.send("Invalid Language: \"" + lang + "\"");
 	}
 	else if (diff == "easy" || diff == "medium" || diff == "hard") {
-		client.query('SELECT * FROM words WHERE language = \'' + lang + '\' AND difficulty = \'' + diff + '\' ORDER BY random() LIMIT 22;', (err, res) => {
-			done();
-			if (err)
-			{ console.error(err); response.send("Error " + err); }
-			else
-			{
-				var retrievedWords = [];
-				for (var i = 0; i < res.rows.length; i++) {
-					retrievedWords.push(res.rows[i]["word"]);
+		pool.connect((err,client, done) => {
+			if (err) throw err;
+			client.query('SELECT * FROM words WHERE language = \'' + lang + '\' AND difficulty = \'' + diff + '\' ORDER BY random() LIMIT 22;', (err, res) => {
+				done();
+				if (err)
+				{ console.error(err); response.send("Error " + err); }
+				else
+				{
+					var retrievedWords = [];
+					for (var i = 0; i < res.rows.length; i++) {
+						retrievedWords.push(res.rows[i]["word"]);
+					}
+					response.send(JSON.stringify(retrievedWords)); 
 				}
-				response.send(JSON.stringify(retrievedWords)); 
-			}
+			});
 		});
 	} else
 		response.send("Invalid Difficulty!");
